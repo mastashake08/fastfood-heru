a54e31ae1dee43e13deeace58eee41e2c3d43f6a
Jyrone Parker <jyrone.parker@gmail.com>
2017-11-17 09:30:16 -0500

    custom order

--- resources/views/admin/home.blade.php
+++ resources/views/admin/home.blade.php
@@ -84,5 +84,85 @@
             </div>
         </div>
     </div>
+    <div class="row">
+        <div class="col-md-8 col-md-offset-2">
+            <div class="panel panel-default">
+                <div class="panel-heading">Place A Custom Order</div>
+
+                <div class="panel-body">
+                  <script src='https://js.stripe.com/v2/' type='text/javascript'></script>
+                  <form accept-charset="UTF-8" action="{{url('/charge')}}" method="POST" class="require-validation" data-cc-on-file="false" data-stripe-publishable-key="{{env('STRIPE_KEY')}}" id="payment-form">
+                    <label class='control-label'>Delivery Address</label>
+                    <input type="text" name="message" value="">
+                    <label class='control-label'>Total Price</label>
+                    <input type="text" name="total_price" value="">
+                    <label class='control-label'>Price</label>
+                    <input type="text" name="price" value="">
+                    <label class='control-label'>Tax</label>
+                    <input type="text" name="tax" value="">
+                    <label class='control-label'>Fee</label>
+                    <input type="text" name="fee" value="">
+                    <div class='form-row'>
+                      <div class='col-xs-12 form-group required'>
+                        <label class='control-label'>Delivery Address</label>
+                        <input class='form-control' name="address" size='4' type='text'>
+                      </div>
+                      <div class='col-xs-12 form-group '>
+                        <label class='control-label'>Comments</label>
+                        <textarea class="form-control" name="comments" placeholder="Extra napkins, no ice, etc."></textarea>
+                        <input class="form-control" placeholder="Tip" name="tip"/>
+                      </div>
+                    </div>
+                    <div class='form-row'>
+                      <div class='col-xs-12 form-group required'>
+                        <label class='control-label'>Name on Card</label>
+                        <input class='form-control' size='4' type='text'>
+                      </div>
+                    </div>
+                    <div class='form-row'>
+                      <div class='col-xs-12 form-group card required'>
+                        <label class='control-label'>Card Number</label>
+                        <input autocomplete='off' class='form-control card-number' size='20' type='text'>
+                      </div>
+                    </div>
+                    <div class='form-row'>
+                      <div class='col-xs-4 form-group cvc required'>
+                        <label class='control-label'>CVC</label>
+                        <input autocomplete='off' class='form-control card-cvc' placeholder='ex. 311' size='4' type='text'>
+                      </div>
+                      <div class='col-xs-4 form-group expiration required'>
+                        <label class='control-label'>Expiration</label>
+                        <input class='form-control card-expiry-month' placeholder='MM' size='2' type='text'>
+                      </div>
+                      <div class='col-xs-4 form-group expiration required'>
+                        <label class='control-label'> </label>
+                        <input class='form-control card-expiry-year' placeholder='YYYY' size='4' type='text'>
+                      </div>
+                    </div>
+                    <div class='form-row'>
+                      <div class='col-md-12'>
+                        <div class='form-control total btn btn-info'>
+                          Total (Delivery Fee Included ):
+                          <span class='amount'>{{money_format('%.2n', $total_price)}}</span>
+                        </div>
+
+                      </div>
+                    </div>
+                    <div class='form-row'>
+                      <div class='col-md-12 form-group'>
+                        <button class='form-control btn btn-primary submit-button' type='submit'>Pay »</button>
+                      </div>
+                    </div>
+                      <div class='col-md-12 error form-group hide'>
+                        <div class='alert-danger alert'>
+                          Please correct the errors and try again.
+                        </div>
+                      </div>
+                    </div>
+                  </form>
+                </div>
+            </div>
+        </div>
+    </div>
 </div>
 @endsection
