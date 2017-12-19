eafd7410c475a26ade0c7c57ba3e50dc51c8fb9c
Jyrone Parker <jyrone.parker@gmail.com>
2017-12-19 15:31:42 -0500

    images

--- resources/views/category/all.blade.php
+++ resources/views/category/all.blade.php
@@ -1,7 +1,7 @@
 @extends('layouts.app')
 
 @section('content')
-@section('background','/img/categories.jpg');
+@section('background','/img/Categories.jpeg');
 <div class="container">
     <div class="row">
         <div class="col-md-8 col-md-offset-2">
