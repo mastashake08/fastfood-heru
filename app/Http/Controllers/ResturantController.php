27293c5dc80cc64339a6ce438d0668429e0f853a
Jyrone Parker <jyrone.parker@gmail.com>
2018-01-05 06:28:14 -0500

    debug

--- app/Http/Controllers/ResturantController.php
+++ app/Http/Controllers/ResturantController.php
@@ -180,7 +180,7 @@ class ResturantController extends Controller
 
     public function search(Request $request){
       $name = $request->name;
-      dd($name);
+      //dd($name);
       $resturants = Resturant::where('name','like',"%{$name}%")->orderBy('name','asc')->paginate(10);
       $with = [
         'resturants' => $resturants
